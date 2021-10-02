bb8967d17d8940cb43dffb0780470b639610d570
Nikola Tankovic <ntankovic@ntankovic-macbook-air.local>
2021-10-02 17:25:03 +0200

    Select a process to run

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="home">
         <div class="position-absolute top-50 start-50 translate-middle">
-            <button class="btn btn-primary" v-on:click="start_bpmn_proces">Start process</button>
+            <select class="form-select" aria-label="Process" v-model="selected_model">
+                <option value="">Odaberi proces...</option>
+                <option v-for="m in models" v-bind:key="m.model_path" :value="m.model_path">
+                    {{ m.main_process.name }}
+                </option>
+            </select>
+            <br />
+            <br />
+            <button class="btn btn-primary" v-on:click="start_bpmn_proces">Start</button>
         </div>
     </div>
 </template>
@@ -14,13 +22,22 @@ export default {
     components: {},
     data() {
         return {
+            selected_model: '',
             instance: '',
             task: '',
+            models: [],
         };
+    },
+    async mounted() {
+        let r = await fetch(`${process.env.VUE_APP_BPMN_SERVER}/model`);
+        this.models = (await r.json()).results;
     },
     methods: {
         start_bpmn_proces() {
-            fetch(`${process.env.VUE_APP_BPMN_SERVER}/instance`, {
+            if (!this.selected_model) {
+                return;
+            }
+            fetch(`${process.env.VUE_APP_BPMN_SERVER}/model/${this.selected_model}/instance`, {
                 method: 'POST',
             })
                 .then((response) => {
